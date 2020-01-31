1e1c13cfc305c5664a1718f88b8899212cff548b
ahayes3 <ahayes5@oswego.edu>
2020-01-31 01:11:17 -0500

    First Commit

---
+++ desktop/src/com/ahayes5/homework1/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.ahayes5.homework1.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.ahayes5.homework1.Homework;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Homework(), config);
+	}
+}
